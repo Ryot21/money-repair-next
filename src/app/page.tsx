c20f36bcfc5f4ad149458127d93b7aa79d2f10bd
Ryot21 <rh.bicmac@gmail.com>
2025-01-10 01:23:29 +0900

    コンポーネント > swiper追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
 
 import MainVisual from "@/components/MainVisual";
-
+import LogoSwiper from "@/components/Swiper/Logo";
 
 export default function Home() {
   return (
     <main>
       <MainVisual />
+      <div id="logoSection" className={"c-contents pdt5 pdt10s mgb5s"}>
+            <div className={"c-contents--inner"}>
+                <h2 className={"s-L -center -b pdb3 pdb3s"}>多くのお客様にご好評いただいております</h2>
+            </div>
+            <div className={"swiper swiper-Logo -right mgb1 mgb3s"}>
+              <LogoSwiper />
+            </div>
+            <div className={"swiper swiper-Logo -left"}>
+              <LogoSwiper />
+            </div>
+      </div>
     </main>
   );
 }
