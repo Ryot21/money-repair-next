334bd967eb5f401529081ce7bb382dc244046ded
Ryot21 <rh.bicmac@gmail.com>
2025-01-14 11:38:55 +0900

    下層ページ > ページタイトルの変数化

--- src/components/MainVisual/single/index.tsx
+++ src/components/MainVisual/single/index.tsx
@@ -1,15 +1,16 @@
 
 type Props = {
     title: string;
+    customClass?: string;
 }
 
-export default function SingleMainVisual({ title } : Props){
+export default function SingleMainVisual({ title, customClass } : Props){
     return(
         <div className={"c-contents -imgBg -fv -single"}>
             <div className={"imgBox"}></div>
             <div className={"c-contents--inner"}>
                 {/* ページタイトル */}
-                <h1 className={"c-page-title -interview s-L -s20 -center -b -ls-2"}>{title}</h1>
+                <h1 className={`c-page-title s-L -s20 -center -b -ls-2 ${customClass || ''} `}>{title}</h1>
                 {/* パンくず 今後実装予定 */}
 
             </div>
