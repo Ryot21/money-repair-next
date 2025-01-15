09e464407c65f88645277877d0e66f23f3457da0
Ryot21 <rh.bicmac@gmail.com>
2025-01-16 01:00:06 +0900

    定数 > コンテンツ表示数を設定
    ・ご利用者の声
    ・お役立ち情報
    ・お知らせ（未設定）

---
+++ src/constants/index.ts
@@ -0,0 +1,11 @@
+
+// ご利用者の声
+export const INTERVIEW_TOP_LIST_LIMIT = 3;
+export const INTERVIEW_PAGE_LIST_LIMIT = 9;
+
+// お役立ち情報
+export const INFORMATION_TOP_LIST_LIMIT = 3;
+export const INFORMATION_PAGE_LIST_LIMIT = 9;
+
+// お知らせ
+export const NEWS_TOP_LIST_LIMIT = 3;
