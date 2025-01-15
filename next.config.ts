685d153ac5cf6e7990c7ae12e9c822afe1b2e269
Ryot21 <rh.bicmac@gmail.com>
2025-01-15 15:26:29 +0900

    microCMS経由で外部画像の読み込みを許可

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,  // React Strict Mode を有効にするオプション（例）
   // 他の設定オプションをここに追加
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.microcms-assets.io",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
