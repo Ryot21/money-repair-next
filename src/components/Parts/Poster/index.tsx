c25dad52994b0adbbd23e28c0dcc1d5cb55b91a1
Ryot21 <rh.bicmac@gmail.com>
2025-01-20 14:33:03 +0900

    コンポーネント > Poster（投稿者）追加

---
+++ src/components/Parts/Poster/index.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Poster } from "@/libs/microcms";
+
+type Props = {
+    poster: Poster[];
+}
+
+export default function Poster({ poster } : Props) {
+    return (
+        <>
+            { poster.map((item, index) => (
+                <div key={index} className={"c-card -t01 mgb10 mgb10s"}>
+                    <ul className={"c-card-lists"}>
+                        <li className={"c-card-item"}>
+                            <div className="imgBox">
+                                {item && (
+                                    <Image 
+                                        src={item.image.url}
+                                        alt={item.name}
+                                        width={150}
+                                        height={150}
+                                    />
+                                )}
+                            </div>
+                        </li>
+                        <li className={"c-card-item"}>
+                            <span className={"postman"}>この記事を書いた人</span>
+                            <p className={"name"}>{item.name}
+                                <br className={"sp"} />
+                                <span className={"jobTitle"}>{item.officialPosition}</span>
+                            </p>
+                            <p>{item.description}</p>
+                            <Link href={`${item.url}`} className={"c-link -logo"}>{item.url}</Link>
+                        </li>
+                    </ul>
+                </div>
+            ))}
+        </>
+    )
+}
