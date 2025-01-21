bb3fc162baed405e64fb056e24a5c59bf40e8417
Ryot21 <rh.bicmac@gmail.com>
2025-01-21 17:07:22 +0900

    お役立ち情報 > カテゴリーページ追加

---
+++ src/app/information/category/[id]/page.tsx
@@ -0,0 +1,47 @@
+
+import { getInformationCategoryDetail, getInformationList } from "@/libs/microcms";
+import { INFORMATION_PAGE_LIST_LIMIT } from "@/constants";
+import { notFound } from "next/navigation";
+import InformationList from "@/features/PostList/Information";
+import CategoryList from "@/components/Parts/Category/List";
+import Category from "@/components/Parts/Category";
+
+type Props = {
+    params: {
+        id: string;
+    };
+};
+
+export default async function Page({ params } : Props){
+        const category = await getInformationCategoryDetail(params.id).catch(notFound);
+
+        const { contents : information } = await getInformationList({
+                filters: `category[equals]${category.id}`,
+                limit: INFORMATION_PAGE_LIST_LIMIT,
+        });
+
+    return (
+        <>
+            {/* 導入部分 */}
+            <div className={"c-contents pdt2 pdt10s pdb5 pdb10s"}>
+                <div className={"c-contents--inner"}>
+                    <p className={"s-ML -s14 -center -ls-2 -lh-2"}>
+                        マネーリペアに関連する<span className={"s-ML -s14 -b -ls-2"}>資産形成</span>に役立つ記事を発信しています。<br className={"tb-pc"} />
+                        従業員の<span className={"s-ML -s14 -b -ls-2"}>経済力向上や福利厚生強化</span>に向けた知識を<br className={"tb-pc"} />
+                        様々な情報を活用して、より豊かな未来を目指しましょう。
+                    </p>
+                </div>
+            </div>
+
+            {/* カテゴリ一覧 */}
+            {/* article = information or interview */}
+            <CategoryList article="information" />
+
+            {/* 記事一覧 */}
+            <div className="c-contents pdt5 pdt10s pdb5 pdb10s">
+                <p><Category category={category}/>の一覧</p>
+                <InformationList contents={information} />
+            </div>
+        </>
+    )
+}
