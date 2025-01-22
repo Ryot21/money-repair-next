d2bf30f6148ec77956587d5ea09c9122f40fbed8
Ryot21 <rh.bicmac@gmail.com>
2025-01-23 03:07:00 +0900

    ご利用者の声 > 一覧ページ追加

---
+++ src/app/interview/layout.tsx
@@ -0,0 +1,13 @@
+import Sheet from "@/components/Sheet"
+
+type Props = {
+    children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: Props) {
+    return (
+        <Sheet title="ご利用者の声" mvClass="-interview" contentClass="-min1200">
+            {children}
+        </Sheet>
+    )
+}
