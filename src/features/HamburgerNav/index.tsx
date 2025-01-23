d0c3fdfe2f70a04b579fc06f4a6612c552e1dfb0
Ryot21 <rh.bicmac@gmail.com>
2025-01-24 03:44:38 +0900

    hNav > ご利用者の声、お役立ち情報 > カテゴリー別リンク追加

--- src/features/HamburgerNav/index.tsx
+++ src/features/HamburgerNav/index.tsx
@@ -1,6 +1,12 @@
 import Link from "next/link";
+import type { Category } from "@/libs/microcms";
+import { getInterviewCategoryList, getInformationCategoryList } from "@/libs/microcms";
 
-export default function HamburgerNav() {
+export default async function HamburgerNav() {
+    // カテゴリー情報を取得
+    const interviewCategories = await getInterviewCategoryList();
+    const informationCategories = await getInformationCategoryList();
+
     return (
         <nav className={"c-hb-nav sp"}>
             <input className={"c-hb-nav__cheakbox"} id="c-hb-nav__checkbox" type="checkbox"/>
@@ -18,24 +24,32 @@ export default function HamburgerNav() {
                     </li>
                     <li className={"hbNavItem c-borderBtm -dotted "}>
                         <Link className={"mgb5 mgb5s"} href="/interview"><span className={"s-ML -s20 -b -white -ls-2 pdr5 pdr4s"}>ご利用者の声</span></Link>
-                        <ul className={"c-flex -col2 -jc-end"}>
-                            <li className={"flexItem mgb5 mgb5s"}><Link className={"s-M -s14 -b -white -center"} href="">カテゴリー01</Link></li>
-                            <li className={"flexItem mgb5 mgb5s"}><Link className={"s-M -s14 -b -white -center"} href="">カテゴリー01</Link></li>
-                            <li className={"flexItem mgb5 mgb5s"}><Link className={"s-M -s14 -b -white -center"} href="">カテゴリー01</Link></li>
-                            <li className={"flexItem mgb5 mgb5s"}><Link className={"s-M -s14 -b -white -center"} href="">カテゴリー01</Link></li>
-                            <li className={"flexItem mgb5 mgb5s"}><Link className={"s-M -s14 -b -white -center"} href="">カテゴリー01</Link></li>
-                            <li className={"flexItem mgb5 mgb5s"}><Link className={"s-M -s14 -b -white -center"} href="">カテゴリー01</Link></li>
-                        </ul>
+                        <ul className={"c-flex -col2 -jc-st"}>
+                            {interviewCategories.contents.map((category: Category) => (
+                                <li key={category.id} className={"flexItem mgb5 mgb5s"}>
+                                    <Link 
+                                        className={"s-M -s14 -b -white -left pdl20s"} 
+                                        href={`/interview/category/${category.id}`}
+                                    >
+                                        {category.name}
+                                    </Link>
+                                </li>
+                        ))}
+                            </ul>
                     </li>
                     <li className={"hbNavItem c-borderBtm -dotted "}>
                         <Link className={"mgb5 mgb5s"} href="/information"><span className={"s-ML -s20 -b -white -ls-2 pdr5 pdr4s"}>お役立ち情報</span></Link>
-                        <ul className={"c-flex -col2 -jc-end"}>
-                            <li className={"flexItem mgb5 mgb5s"}><Link className={"s-M -s14 -b -white -center"} href="">カテゴリー01</Link></li>
-                            <li className={"flexItem mgb5 mgb5s"}><Link className={"s-M -s14 -b -white -center"} href="">カテゴリー01</Link></li>
-                            <li className={"flexItem mgb5 mgb5s"}><Link className={"s-M -s14 -b -white -center"} href="">カテゴリー01</Link></li>
-                            <li className={"flexItem mgb5 mgb5s"}><Link className={"s-M -s14 -b -white -center"} href="">カテゴリー01</Link></li>
-                            <li className={"flexItem mgb5 mgb5s"}><Link className={"s-M -s14 -b -white -center"} href="">カテゴリー01</Link></li>
-                            <li className={"flexItem mgb5 mgb5s"}><Link className={"s-M -s14 -b -white -center"} href="">カテゴリー01</Link></li>
+                        <ul className={"c-flex -col2 -jc-st"}>
+                            {informationCategories.contents.map((category: Category) => (
+                                <li key={category.id} className={"flexItem mgb5 mgb5s"}>
+                                    <Link 
+                                        className={"s-M -s14 -b -white -left pdl20s"} 
+                                        href={`/information/category/${category.id}`}
+                                    >
+                                        {category.name}
+                                    </Link>
+                                </li>
+                            ))}
                         </ul>
                     </li>
                     <li className={"hbNavItem "}>
