a54fe55b840180fbf2326eaeecc2b9432b82449e
Ryot21 <rh.bicmac@gmail.com>
2025-01-24 02:54:30 +0900

    ご利用者の声 > 一覧ページに「カテゴリー & ページネーション」追加

---
+++ src/app/interview/category/[id]/page.tsx
@@ -0,0 +1,70 @@
+import {
+  getInterviewCategoryDetail,
+  getInterviewsList,
+  getInterviewCategoryList,
+} from "@/libs/microcms";
+import { INTERVIEW_PAGE_LIST_LIMIT } from "@/constants";
+import { notFound } from "next/navigation";
+import InterviewList from "@/features/PostList/Interview";
+import CategoryList from "@/components/Parts/Category/List";
+import Pagination from "@/components/Parts/pagination";
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+export default async function Page({ params }: Props) {
+  const category = await getInterviewCategoryDetail(params.id).catch(
+    notFound
+  );
+  const categories = await getInterviewCategoryList();
+
+  const { contents: interview, totalCount } = await getInterviewsList({
+    filters: `category[equals]${category.id}`,
+    limit: INTERVIEW_PAGE_LIST_LIMIT,
+  });
+
+  return (
+    <>
+      {/* 導入部分 */}
+      <div className={"c-contents pdt2 pdt10s pdb5 pdb10s"}>
+        <div className={"c-contents--inner"}>
+          <p className={"s-ML -s14 -center -ls-2 -lh-2"}>
+            マネーリペアに関連する
+            <span className={"s-ML -s14 -b -ls-2"}>資産形成</span>
+            に役立つ記事を発信しています。
+            <br className={"tb-pc"} />
+            従業員の
+            <span className={"s-ML -s14 -b -ls-2"}>
+              経済力向上や福利厚生強化
+            </span>
+            に向けた知識を
+            <br className={"tb-pc"} />
+            様々な情報を活用して、より豊かな未来を目指しましょう。
+          </p>
+        </div>
+      </div>
+
+      {/* カテゴリ一覧 */}
+      {/* article = information or interview */}
+      <CategoryList
+        article="interview"
+        currentCategoryId={params.id}
+        categories={categories}
+      />
+
+      {/* 記事一覧 */}
+      <div className="c-contents pdt5 pdt10s pdb5 pdb10s">
+        <InterviewList contents={interview} />
+      </div>
+
+      {/* ページネーション */}
+      <Pagination
+        totalCount={totalCount}
+        basePath={`/interview/category/${category.id}`}
+      />
+    </>
+  );
+}
---
+++ src/app/interview/page/[current]/page.tsx
@@ -0,0 +1,61 @@
+import { notFound } from "next/navigation";
+import { getInterviewsList, getInterviewCategoryList } from "@/libs/microcms";
+import InterviewList from "@/features/PostList/Interview";
+import { INTERVIEW_PAGE_LIST_LIMIT } from "@/constants"
+import CategoryList from "@/components/Parts/Category/List";
+import Pagination from "@/components/Parts/pagination";
+
+type Props = {
+    params: {
+        current: string;
+    }
+}
+
+export default async function Page({ params }: Props) {
+    const current = parseInt(params.current, 10);
+
+    if (Number.isNaN(current) || current < 1) {
+        notFound();
+    }
+
+    const { contents: interview, totalCount } = await getInterviewsList({
+        limit: INTERVIEW_PAGE_LIST_LIMIT,
+        offset: INTERVIEW_PAGE_LIST_LIMIT * (current -1),
+    });
+
+    if (interview.length === 0){
+        notFound();
+    }
+    const categories = await getInterviewCategoryList();
+    return (
+
+        <>
+            {/* 導入部分 */}
+            <div className={"c-contents pdt2 pdt10s pdb5 pdb10s"}>
+                <div className={"c-contents--inner"}>
+                    <p className={"s-ML -s14 -center -ls-2 -lh-2"}>
+                        マネーリペアをご利用された方々の<span className={"s-ML -s14 -b -ls-2"}>率直な体験談</span>を集めました。<br className={"tb-pc"} />
+                        実際の体験を通じて、<span className={"s-ML -s14 -b -ls-2"}>どのように金融知識が深まり、</span><br className={"tb-pc"} />
+                        <span className={"s-ML -s14 -b -ls-2"}>経済的な安心が得られたのかをご紹介します。</span><br className={"tb-pc"} />
+                        ぜひマネーリペアが提供する価値をご確認ください。
+                    </p>
+                </div>
+            </div>
+            {/* カテゴリ一覧 */}
+            <CategoryList
+                article="interview"
+                categories={categories}
+            />
+            {/* 記事一覧 */}
+            <div className="c-contents pdt5 pdt10s pdb5 pdb10s">
+                <InterviewList contents={ interview }/>
+            </div>
+            {/* ページネーション */}
+            <Pagination
+                totalCount={totalCount}
+                current={current}
+                basePath={`/interview`}
+            />
+        </>
+    )
+}
