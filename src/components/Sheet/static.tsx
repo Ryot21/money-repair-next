917ef02fc7e47ad916e24186b431c9cf3e8d06d1
Ryot21 <rh.bicmac@gmail.com>
2025-02-10 17:57:35 +0900

    下層レイアウトに静的ページ用の「Static」を追加

---
+++ src/components/Sheet/static.tsx
@@ -0,0 +1,25 @@
+// 共通レイアウト
+import SingleMainVisual from "@/components/MainVisual/single";
+
+type Props = {
+    children: React.ReactNode;
+    title: string;
+    mvClass?: string;
+    contentClass?: string;
+}
+
+export default function Sheet({ children, title, mvClass, contentClass }: Props) {
+    return (
+        <main>
+            {/* メインビジュアル */}
+            <SingleMainVisual title={title} mvClass={mvClass}/>
+
+            {/* コンテンツ */}
+            <div className={`c-contents pdt5 pdt10s pdb5 pdb10s ${contentClass || ''} `} >
+                <div className="c-contents--inner -bg-white -card">
+                    {children}
+                </div>
+            </div>
+        </main>
+    );
+}
