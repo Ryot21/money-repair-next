f68e5e82d48f403dd3a944d3782b4bedd77b9d6d
Ryot21 <rh.bicmac@gmail.com>
2025-02-14 00:44:33 +0900

    Update index.tsx

--- src/features/Article/Information/index.tsx
+++ src/features/Article/Information/index.tsx
@@ -1,6 +1,5 @@
 
 import Image from "next/image";
-import Link from "next/link";
 import type { Information } from "@/libs/microcms";
 import Date from "@/components/Parts/Date";
 import CategoryLink from "@/components/Parts/Category/Link";
