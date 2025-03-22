95a794711f946fda67946434164dbe6ba7121bac
Ryot21 <rh.bicmac@gmail.com>
2025-03-22 14:49:59 +0900

    運営企業ページ > レイアウト調整

--- src/app/(media)/company/page.tsx
+++ src/app/(media)/company/page.tsx
@@ -18,11 +18,11 @@ export default function Page() {
                         <dd>〒101-0032<br />東京都千代田区岩本町2-19-9　丸栄ビル5階</dd>
                     </div>
                     <div className="p-company__list">
-                        <dt>TEL / FAX</dt>
+                        <dt>TEL / <br className="sp" />FAX</dt>
                         <dd>03-6264-7047 / 03-6264-7048</dd>
                     </div>
                     <div className="p-company__list">
-                        <dt>メールアドレス</dt>
+                        <dt>メール<br className="sp" />アドレス</dt>
                         <dd>info@impreme.jp</dd>
                     </div>
                     <div className="p-company__list">
@@ -42,10 +42,10 @@ export default function Page() {
                         </dd>
                     </div>
                     <div className="p-company__list">
-                        <dt>所属金融商品取引業者</dt>
+                        <dt>所属金融商品<br className="sp" />取引業者</dt>
                         <dd>
                             東海東京証券株式会社　東海財務局長（金商）第140号<br />
-加入協会：日本証券業協会、一般社団法人金融先物取引業協会、一般社団法人第二種金融商品取引業協会、一般社団法人日本投資顧問業協会、一般社団法人日本STO協会
+                            加入協会：日本証券業協会、一般社団法人金融先物取引業協会、一般社団法人第二種金融商品取引業協会、一般社団法人日本投資顧問業協会、一般社団法人日本STO協会
                         </dd>
                     </div>
                 </dl>
