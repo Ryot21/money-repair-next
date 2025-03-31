d5e23dd2fac2a65bd544a33995587b295fb89f8f
Ryot21 <rh.bicmac@gmail.com>
2025-03-31 11:08:52 +0900

    ナビゲーションエラー解消

--- src/features/media/navigation/NavigationContainer.tsx
+++ src/features/media/navigation/NavigationContainer.tsx
@@ -1,18 +1,18 @@
-import { getInterviewCategoryList } from "@/features/media/api/microcms/getInterview";
-import { getInformationCategoryList } from "@/features/media/api/microcms/getInformation";
+// import { getInterviewCategoryList } from "@/features/media/api/microcms/getInterview";
+// import { getInformationCategoryList } from "@/features/media/api/microcms/getInformation";
 import Navigation from ".";
 
 // このコンポーネントはサーバーサイドでデータを取得し、
 // クライアントコンポーネントであるHamburgerNavContentにデータを渡す役割を持ちます
 export default async function NavigationContainer() {
   // microCMSからカテゴリー情報を取得
-  const interviewCategories = await getInterviewCategoryList();
-  const informationCategories = await getInformationCategoryList();
+  // const interviewCategories = await getInterviewCategoryList();
+  // const informationCategories = await getInformationCategoryList();
 
   return (
     <Navigation
-      interviewCategories={interviewCategories}
-      informationCategories={informationCategories}
+      // interviewCategories={interviewCategories}
+      // informationCategories={informationCategories}
     />
   );
 }
