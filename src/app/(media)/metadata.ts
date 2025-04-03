648d427fd630dde5adbea01e972829731a037133
Ryot21 <rh.bicmac@gmail.com>
2025-04-04 02:24:17 +0900

    メディア > カノニカル設定解除

--- src/app/(media)/metadata.ts
+++ src/app/(media)/metadata.ts
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
       "金融は分野によって知識が偏りがちなので、金融知識という知的財産を身につけ、自分たちで適切な判断ができるようにすることが必要だと感じます。金融リテラシーを上げることにより「将来に向けたお金の不安を解決する」サービスです。",
     images: ["/images/sns/ogp_1200_630.jpg"],
   },
-  alternates: {
-    canonical: "https://money-repair.jp",
-  },
+  // alternates: {
+  //   canonical: "https://money-repair.jp",
+  // },
 };
