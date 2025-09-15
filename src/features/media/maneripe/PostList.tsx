d91a1840bb4ac5233f1586ce3f7abec343b6b691
Ryot21 <rh.bicmac@gmail.com>
2025-09-16 02:48:20 +0900

    Update PostList.tsx

--- src/features/media/maneripe/PostList.tsx
+++ src/features/media/maneripe/PostList.tsx
@@ -7,7 +7,6 @@ type Props = {
   contents: Maneripe[];
 };
 
-
 export default function ManeripeList({ contents }: Props) {
   if (contents.length === 0) {
     return <p>記事がありません。</p>;
@@ -17,16 +16,17 @@ export default function ManeripeList({ contents }: Props) {
       {contents.map((article) => (
         <li key={article.id} className={"c-post--item mgb1 mgb2s"}>
           <Link href={`/maneripe/${article.id}`} className={"c-post--link"}>
-
             {/* テキストエリア */}
             <div className={"c-post--data"}>
               <div className={"c-date mgb1 mgb1s"}>
                 <ul className={"c-date__lists c-flex -col2"}>
                   <li className={"c-date__item"}>
-                    <Date date={article.publishedAt || article.createdAt} />
+                    <Date
+                      publishedAt={article.publishedAt}
+                      createdAt={article.createdAt}
+                    />
                   </li>
-                  <li className={"c-date__item"}>
-                  </li>
+                  <li className={"c-date__item"}></li>
                 </ul>
               </div>
               <h3 className={"c-post--title s-ML -s16 -left -b -ls-2 -lh-1_5"}>
